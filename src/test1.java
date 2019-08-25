e9d416b4167a2d962671797b0837b4d3b8cf8804
Aswathy Sali <aswathysali1994@gmail.com>
2019-08-25 16:06:27 +0530

    adding for loop to display numbers

--- src/test1.java
+++ src/test1.java
@@ -2,7 +2,11 @@
 public class test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int i=0;
+		for ( i=0;i<10;i++)
+		{
+			System.out.println("The number is :"+i);
+		}
 
 	}
 
