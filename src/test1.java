96614575603842471abb19e7cea030ea8275e182
Aswathy Sali <aswathysali1994@gmail.com>
2019-08-25 16:37:28 +0530

    adding sum

--- src/test1.java
+++ src/test1.java
@@ -3,9 +3,11 @@ public class test1 {
 
 	public static void main(String[] args) {
 		int arr[]= {1,2,3};
+		int sum=0;
 		for (int i :arr )
 		{
 			System.out.println("The number is :"+i);
+			sum+=i;
 		}
 
 	}
