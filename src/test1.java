3bd0cb66a7abc8f3368f5bd6b119aa817b2b933a
Aswathy Sali <aswathysali1994@gmail.com>
2019-08-25 16:16:52 +0530

    third change with index

--- src/test1.java
+++ src/test1.java
@@ -2,8 +2,8 @@
 public class test1 {
 
 	public static void main(String[] args) {
-		int i=0;
-		for ( i=0;i<10;i++)
+		int index=0;
+		for ( int i=0;i<10;i++)
 		{
 			System.out.println("The number is :"+i);
 		}
